fd4dc920acb217c281c097edd17edebd8dd4b0b1
widowslace <haylrose01@gmail.com>
2022-03-10 15:08:55 -0500

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'quotes_controller/create'
   post '/quotes', to: 'quotes#create'
@@ -7,10 +9,9 @@ Rails.application.routes.draw do
   get 'sign_up', to: 'registrations_#new'
   post 'sign_up', to: 'registrations_#create'
 
-  get 'sign_in', to: 'sessions#new'
-  post 'sign_in', to: 'sessions#create', as: 'log_in'
-
-  delete 'logout', to: 'sessions#destroy'
+  get    'sign_in'   => 'sessions#new'
+  post   'sign_in'   => 'sessions#create'
+  delete 'sign_out'  => 'sessions#destroy'
 
   get 'password', to: 'passwords#edit', as: 'edit_password'
   patch 'password', to: 'passwords#update'
@@ -20,8 +21,6 @@ Rails.application.routes.draw do
   get 'password/reset/edit', to: 'password_resets#edit'
   patch 'password/reset/edit', to: 'password_resets#update'
   root 'pages#index'
-  
-    
   
   #route to home page, commercial,residential, and quote
   get '/residential', to: 'pages#residential'

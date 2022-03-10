de5d60a77d9e744144d3f1fb5b15c37dc5fde6d1
markhendrikson <hendriksonmark@gmail.com>
2022-03-10 09:10:42 -0500

    progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
-  # get index => 'pages#index'
-  # get residential => 'pages#residential'
-  # get commercial => 'pages#commercial'
-  # get quote => 'pages#quote'
+  
+    
+  
+  #route to home page, commercial,residential, and quote
+  get 'index' => 'pages#index'
+  get 'residential' => 'pages#residential'
+  get 'commercial' => 'pages#commercial'
+  get 'quote' => 'pages#quote'
+
+
+
 end

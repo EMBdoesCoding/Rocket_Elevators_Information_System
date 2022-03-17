eb58b34f0978c00748bda131efcf42f2d5b87f40
Digby Laret <dlaret99@gmail.com>
2022-03-17 14:31:13 -0400

    added converter

--- lib/tasks/converter.rb
+++ lib/tasks/converter.rb
@@ -1,24 +1,22 @@
-# require 'dbi'
-# require 'dbd/mysql'
-# require 'dbd/pg'
+#!/usr/bin/ruby
+# frozen_string_literal: true
 
-# begin
-#   mysql = DBI.connect("DBI:Mysql:source:localhost", "username", "password")
-#   postgres = DBI.connect("DBI:Pg:destination:localhost", "username", "password")
+require 'mysql2'
+require 'pg'
 
-#   mysql.select_all("SHOW TABLES") do |table|
-#     next if ['schema_migrations', 'sessions'].include?(table.to_s)
-#     select = mysql.execute("SELECT * FROM #{table}")
-#     columns = select.column_names.map { |key| ""#{key}"" }.join(', ')
-#     placeholders = (['?'] * select.column_names.size).join(', ')
-#     insert = postgres.prepare("INSERT INTO #{table} (#{columns}) VALUES(#{placeholders})")
-#     select.each { |row| insert.execute(*row) }
-#     insert.finish
-#   end
-# rescue DBI::DatabaseError => e
-#   puts "Error #{e.err}: #{e.errstr}"
-# ensure
-#   mysql.disconnect if mysql
-#   postgres.disconnect if postgres
-# end
-puts "hellow owrl"
+class DBConnection
+  def pg_connect
+    PG.connect( :hostaddr=>"localhost", :port=>5432, 
+      :dbname=>"database", :user=>"postgres_user", :password=>'postgres_password')
+  end
+  
+  def mysql_connect
+    Mysql2::Client.new(:host => "localhost", :username => "root",
+      :password => "Lare1213!", :database => "database")
+  end
+end
+
+quotes.all.each do |//row|
+    FactQuote.create!(
+
+    )
